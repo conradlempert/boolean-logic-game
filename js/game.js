12e91dab5bcb56d684cf21d01a1b494766d4f66f
Conrad Lempert <conradlempert@Conrads-MacBook-Pro.local>
2017-11-25 13:27:16 +0100

    GameElements, grid, and not working drawConnections

--- js/game.js
+++ js/game.js
@@ -1,4 +1,5 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var gridUnit = 25;
 
 function preload() {
 	game.load.image('logo', 'img/openhpi.jpg');
@@ -9,17 +10,28 @@ function preload() {
 
 function create() {
 	//game.add.sprite(100, 100, 'logo');
-	gate1 = new Gate('and', 50, 50);
-	input1 = new Input(50, 150, true);
-	input1.show();
+	ourGame = new GameElements();
+	input1 = new Input(2, 6, true);
+	gate1 = new Gate('and', 8, 2);
+	
+	ourGame.gates.push(gate1);
+	ourGame.inputs.push(input1);
+
+	input1.nextGates.push(gate1);
 }
 
 function update() {
+
+	for(var i = 0; i < ourGame.inputs.length; i++) {
+		ourGame.inputs[i].drawConnections();
+	}
 }
+
 
 function Gate(type, x, y) {
 	this.type = type;
-	this.sprite = game.add.sprite(x, y, type);
+	this.sprite = game.add.sprite(x * 25, y * 25, type);
+	this.nextGates = [];
 }
 
 function Input(x, y, defaultOn) {
@@ -37,10 +49,29 @@ function Input(x, y, defaultOn) {
 		this.show();
 	}
 
+	this.drawConnections = function () {
+		for(var i = 0; i < this.nextGates.length; i++) {
+			var graphics = game.add.graphics(100, 100);
+			graphics.lineStyle(10, 0xffd900, 1);
+			graphics.moveTo(50,50);
+    		graphics.lineTo(250, 50);
+    		graphics.lineTo(100, 100);
+    		graphics.lineTo(250, 220);
+    		graphics.lineTo(50, 220);
+    		graphics.lineTo(50, 50);
+    		window.graphics = graphics;
+		}
+	}
+
 	this.on = defaultOn;
-	this.sprite = game.add.sprite(x, y, 'on');
+	this.nextGates = [];
+	this.sprite = game.add.sprite(x * 25, y * 25, 'on');
 	this.show();
-
-
 	
+}
+
+function GameElements() {
+	this.gates = [];
+	this.inputs = [];
+	this.outputs = [];
 }
