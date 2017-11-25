3e63634910c8f4c3300a282109465b160eb2d3d4
Conrad Lempert <conradlempert@Conrads-MacBook-Pro.local>
2017-11-25 16:11:22 +0100

    removed merge legacy

--- js/input.js
+++ js/input.js
@@ -4,7 +4,6 @@ var Input = function (x, y, on) {
     this.on = on || true;
     this.x = x * gridUnit;
     this.y = y * gridUnit;
-    this.nextGates = [];
     this.sprite = game.add.sprite(this.x, this.y, 'on');
 
     this.updated = new Phaser.Signal();
@@ -24,26 +23,7 @@ var Input = function (x, y, on) {
         this.updated.dispatch();
     };
 
-    this.drawConnections = function () {
-        for(var i = 0; i < this.nextGates.length; i++) {
-            var startX = this.x + 25;
-            var startY = this.y + 12;
-            var goalX = this.nextGates[i].x;
-            var goalY = this.nextGates[i].y + 25;
-            var midX = (startX + goalX) / 2;
-            var graphics = game.add.graphics(0, 0);
-            graphics.lineStyle(3, 0xffd900, 1);
-            graphics.moveTo(startX, startY);
-            graphics.lineTo(midX, startY);
-            graphics.lineTo(midX, goalY);
-            graphics.lineTo(goalX, goalY);
-
-            window.graphics = graphics;
-        }
-    };
-
     this.addChild = function (child) {
-        this.nextGates.push(child);
         child.register(this);
         this.updated.dispatch();
     };
