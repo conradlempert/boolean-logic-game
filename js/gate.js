c24a30680144f5011cee24622ae522ebdb713383
Conrad Lempert <conradlempert@Conrads-MacBook-Pro.local>
2017-11-25 14:52:57 +0100

    merge 2

--- js/gate.js
+++ js/gate.js
@@ -2,6 +2,27 @@ var Gate = function (type, x, y) {
     this.x = x * gridUnit;
     this.y = y * gridUnit;
     this.type = type;
+    this.inputs = [];
     this.sprite = game.add.sprite(this.x, this.y, type);
     this.nextGates = [];
+
+    this.drawConnections = function () {
+        for(var i = 0; i < this.inputs.length; i++) {
+        	console.log(this.inputs[i]);
+            var startX = this.inputs[i].x + 25;
+            var startY = this.inputs[i].y + 12;
+            var goalX = this.x;
+            var goalY = this.y + 25;
+            var midX = (startX + goalX) / 2;
+            var graphics = game.add.graphics(0, 0);
+            graphics.lineStyle(3, 0xffd900, 1);
+            graphics.moveTo(startX, startY);
+            graphics.lineTo(midX, startY);
+            graphics.lineTo(midX, goalY);
+            graphics.lineTo(goalX, goalY);
+
+            window.graphics = graphics;
+        }
+    }
+
 }
