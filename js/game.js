038fc4d65234c39e001eea19d6f700d39e765ce6
Conrad Lempert <conradlempert@Conrads-MacBook-Pro.local>
2017-11-25 13:49:16 +0100

    changed grid system, added real connections

--- js/game.js
+++ js/game.js
@@ -12,7 +12,7 @@ function create() {
 	//game.add.sprite(100, 100, 'logo');
 	ourGame = new GameElements();
 	input1 = new Input(2, 6, true);
-	gate1 = new Gate('and', 8, 2);
+	gate1 = new Gate('and', 8, 10);
 	
 	ourGame.gates.push(gate1);
 	ourGame.inputs.push(input1);
@@ -29,8 +29,10 @@ function update() {
 
 
 function Gate(type, x, y) {
+	this.x = x * gridUnit;
+	this.y = y * gridUnit;
 	this.type = type;
-	this.sprite = game.add.sprite(x * 25, y * 25, type);
+	this.sprite = game.add.sprite(this.x, this.y, type);
 	this.nextGates = [];
 }
 
@@ -51,21 +53,27 @@ function Input(x, y, defaultOn) {
 
 	this.drawConnections = function () {
 		for(var i = 0; i < this.nextGates.length; i++) {
-			var graphics = game.add.graphics(100, 100);
-			graphics.lineStyle(10, 0xffd900, 1);
-			graphics.moveTo(50,50);
-    		graphics.lineTo(250, 50);
-    		graphics.lineTo(100, 100);
-    		graphics.lineTo(250, 220);
-    		graphics.lineTo(50, 220);
-    		graphics.lineTo(50, 50);
+			var startX = this.x + 25;
+			var startY = this.y + 12;
+			var goalX = this.nextGates[i].x;
+			var goalY = this.nextGates[i].y + 25;
+			var midX = (startX + goalX) / 2;
+			var graphics = game.add.graphics(0, 0);
+			graphics.lineStyle(3, 0xffd900, 1);
+			graphics.moveTo(startX, startY);
+			graphics.lineTo(midX, startY);
+			graphics.lineTo(midX, goalY);
+			graphics.lineTo(goalX, goalY);
+    		
     		window.graphics = graphics;
 		}
 	}
 
+	this.x = x * gridUnit;
+	this.y = y * gridUnit;
 	this.on = defaultOn;
 	this.nextGates = [];
-	this.sprite = game.add.sprite(x * 25, y * 25, 'on');
+	this.sprite = game.add.sprite(this.x, this.y, 'on');
 	this.show();
 	
 }
