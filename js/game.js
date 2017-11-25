33ff4c45b6c3b626579cfb63839340ffa6719ea0
Jacob Freise <jacob.freise@student.hpi.de>
2017-11-25 13:38:29 +0100

    Add output class, toggle input, refactor class definition.

--- js/game.js
+++ js/game.js
@@ -11,36 +11,58 @@ function create() {
 	//game.add.sprite(100, 100, 'logo');
 	gate1 = new Gate('and', 50, 50);
 	input1 = new Input(50, 150, true);
+	var outputs = [Output(true, 'off', 200, 25), Output(false, 'off', 200, 100)];
+	output = new Output(outputs);
 	input1.show();
 }
 
 function update() {
 }
 
-function Gate(type, x, y) {
+var Gate = function (type, x, y) {
 	this.type = type;
 	this.sprite = game.add.sprite(x, y, type);
-}
+};
 
-function Input(x, y, defaultOn) {
+var Input = function (x, y, defaultOn) {
 
+    this.sprite = game.add.sprite(x, y, 'on');
+    this.on = defaultOn;
 	this.show = function () {
 		if(this.on) {
 			this.sprite.loadTexture('on');
 		} else {
 			this.sprite.loadTexture('off');
 		}
-	}
+	};
 
-	this.switch = function () {
-		this.on = !this.on;
-		this.show();
-	}
+	this.toggle = function () {
+        this.on = !this.on;
+        this.show();
+	};
 
-	this.on = defaultOn;
-	this.sprite = game.add.sprite(x, y, 'on');
+    console.log(this.switch);
 	this.show();
 
+    game.input.onDown.add(this.toggle, this);
+};
 
-	
-}
+var Output = function (expected, type, x, y) {
+    this.on = false;
+    this.type = type;
+    this.expected = expected;
+    this.sprite = game.add.sprite(x, y, type);
+
+    this.show = function () {
+        if(this.on) {
+            this.sprite.loadTexture('on');
+        } else {
+            this.sprite.loadTexture('off');
+        }
+    };
+
+    this.switch = function () {
+        this.on = !this.on;
+        this.show();
+    };
+};
