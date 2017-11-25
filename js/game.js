73c32ca42b52c7616e18e299c31cbd74055a3af7
Aron Kunze <aron.kunze@gmail.com>
2017-11-25 12:43:08 +0100

    adding gates.png

--- js/game.js
+++ js/game.js
@@ -1,11 +1,12 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
-	game.load.image('logo', 'img/openhpi.jpg');
+	game.load.image('gates', 'img/gates.png');
 }
 
 function create() {
-	game.add.sprite(100, 100, 'logo');
+	//game.add.sprite(0, 1, 'gates');
+	game.add.tileSprite(0, 0, 330, 170, 'gates')
 }
 
 function update() {
