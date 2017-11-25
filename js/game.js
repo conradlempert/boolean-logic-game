2ab12f5752388a3600c6c81041b93d5c7b2b538d
Conrad Lempert <conradlempert@Conrads-MacBook-Pro.local>
2017-11-25 15:04:15 +0100

    Adaptive lines for multiple connections

--- js/game.js
+++ js/game.js
@@ -19,19 +19,22 @@ function preload() {
 
 function create() {
 	input1 = new Input(2, 2, true);
+	input2 = new Input(2, 10, true);
 	gate1 = new Gate('or', 8, 2);
 
 	gameElements.gates.push(gate1);
 	gameElements.inputs.push(input1);
+	gameElements.inputs.push(input2);
 
 	gate1.inputs.push(input1);
+	gate1.inputs.push(input2);
     gameElements.outputs.push(new Output(true, 'off', 200, 25));
 	gameElements.outputs.push(new Output(false, 'off', 200, 100));
 }
 
 function update() {
 	// Drawing Connectios between
-	for(var i = 0; i < gameElements.inputs.length; i++) {
+	for(var i = 0; i < gameElements.gates.length; i++) {
 		gameElements.gates[i].drawConnections();
 	}
 }
--- js/gate.js
+++ js/gate.js
@@ -9,10 +9,14 @@ var Gate = function (type, x, y) {
     this.drawConnections = function () {
         for(var i = 0; i < this.inputs.length; i++) {
         	console.log(this.inputs[i]);
+        	var goalX = this.x;
+            var goalY = this.y + 13;
+        	if(i == 1) {
+        		goalY = this.y + 37;
+        	}
+        	console.log(goalY);
             var startX = this.inputs[i].x + 25;
             var startY = this.inputs[i].y + 12;
-            var goalX = this.x;
-            var goalY = this.y + 25;
             var midX = (startX + goalX) / 2;
             var graphics = game.add.graphics(0, 0);
             graphics.lineStyle(3, 0xffd900, 1);
